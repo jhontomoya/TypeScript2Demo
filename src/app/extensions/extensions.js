34e7edae01e38f3792e74d5b2e3d25dd645152d2
JAVA3 <jhon_dasher@hotmail.com>
2018-04-17 14:33:00 -0500

    formato fecha

--- src/app/extensions/extensions.js
+++ src/app/extensions/extensions.js
@@ -3,8 +3,15 @@ Date.prototype.addDaystoCurrentDay = function (days) {
     dat.setDate(dat.getDate() + days);
     return dat;
 };
+Date.prototype.format = function (fecha) {
+    let fec = new Date(this.valueOf());
+    var day = fec.getDate();
+    var mounth = fec.getMonth();
+    var year = fec.getFullYear();
+    return `${day}/${++mounth}/${year}`;
+};
 window.onload = function () {
     var currentDay = new Date();
     console.log(currentDay);
-    console.log(currentDay.addDaystoCurrentDay(5));
+    console.log(currentDay.addDaystoCurrentDay(5), currentDay.format(currentDay));
 };
--- src/app/extensions/extensions.ts
+++ src/app/extensions/extensions.ts
@@ -1,5 +1,6 @@
 interface Date {
     addDaystoCurrentDay(days: number):Date;
+    format(fecha: Date):String;
 }
 
 Date.prototype.addDaystoCurrentDay = function(days: number): Date{
@@ -8,8 +9,18 @@ Date.prototype.addDaystoCurrentDay = function(days: number): Date{
     return dat;
 }
 
+Date.prototype.format = function(fecha: Date): String{
+    let fec = new Date(this.valueOf());
+    var day = fec.getDate();
+    var mounth = fec.getMonth();
+    var year = fec.getFullYear();
+    return `${day}/${++mounth}/${year}`;   
+ //return fec.toLocaleDateString();// Solucion facil
+}
+
 window.onload = function(){
     var currentDay = new Date();
     console.log(currentDay);
-    console.log(currentDay.addDaystoCurrentDay(5));
+    console.log(currentDay.addDaystoCurrentDay(5),
+                currentDay.format(currentDay));
 }
